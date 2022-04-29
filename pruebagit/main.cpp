1c49db2c17701b1c53c562354bad942d87d927ba
AllanChen03 <allan031199@gmail.com>
2022-04-28 21:04:24 -0600

    Prueba Allan

---
+++ pruebagit/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void imprimeAllan(){
+    std::cout << "Allan" << std::endl;
+}
+
+int main() {
+    imprimeAllan();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
