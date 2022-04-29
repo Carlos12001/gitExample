5c66497ab1568fddaedfce7d08c9fff926aef6ab
Carlos12001 <62664256+Carlos12001@users.noreply.github.com>
2022-04-28 20:57:27 -0600

    Imprimo el nombre Carlos

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+void imprimeCarlos(){
+    std::cout<<"Hola mi nombre es Carlos" <<std::endl;
+}
 
 int main() {
+    imprimeCarlos();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
